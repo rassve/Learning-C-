c921d55121d4f8b43995b47e8827b8f5bc6a64b8
rassve <49520789+rassve@users.noreply.github.com>
2020-04-05 15:15:02 +0200

    Add files via upload

---
+++ Lecture3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std; 
+
+//example 
+/*
+int power(int base, int exponent){
+    int result = 1; 
+    for(int i = 0; i < exponent; i++){
+        result *= base; 
+    }
+    return result;
+}
+
+int main(){
+    int value = power(3,2);
+    cout << value; 
+}
+*/ 
+
+// if no value return then use the void func. 
+
+// if a function uses another function the function which is used has to be declared before
+ 
+// use header files to call functions from another file 
+
+//<cmath> good library 
+
+
+
+
