88a32bfbbb0ea204d202646679cc1b105c342240
rassve <49520789+rassve@users.noreply.github.com>
2020-04-06 00:52:29 +0200

    Add files via upload

---
+++ Lecture4.cpp
@@ -0,0 +1,16 @@
+//Arrays and Strings 
+
+// "type array_name[dimension]" if it's the same size all the time. i.e. int array[4], don't have to write dimension 
+
+// int array[] = {6, 0, 9, 6}; or int array[4]; array[0] = 6, array[1]... 
+
+//2D array: array_name[dim1][dim2]
+
+// int twoDimArray[2][4] = { { 6, 0, 9, 6 } , { 2, 0, 1, 1 } }; initialization - all dimensions but the first MUST be specified 
+
+// STRINGS - char helloworld[] = “Hello, world!”; 
+
+
+
+
+
